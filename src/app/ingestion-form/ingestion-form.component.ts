f040cc02bab5810f51d13a19a49d4f237e20a297
RyanWYoung <ryan_w_young@hotmail.com>
2017-07-07 16:10:20 -0400

    figuring out how to do dropdown menus for reactive forms

--- src/app/ingestion-form/ingestion-form.component.ts
+++ src/app/ingestion-form/ingestion-form.component.ts
@@ -21,10 +21,14 @@ import { CONCEN_RATIOS_MASS_VOL, CONCEN_RATIOS_VOL_VOL, INTAKE_RATIOS, WEIGHT_RA
 })
 export class IngestionFormComponent implements OnInit {
     toxicology: Toxicology; //TODO: remove
-    concenUnit: Toxicology;
-    intakeUnit: Toxicology;
-    weightUnit: Toxicology;
-    doseUnit: Toxicology;
+    concenTox: Toxicology;
+    intakeTox: Toxicology;
+    weightTox: Toxicology;
+    doseTox: Toxicology;
+    concenUnitsMassVolOptions = CONCEN_RATIOS_MASS_VOL;
+    concenUnitsVolVolOptions = CONCEN_RATIOS_VOL_VOL;
+    intakeUnitOptions = INTAKE_RATIOS;
+    weightUnitsOptions = WEIGHT_RATIOS;
     submitted = false;
     pastToxicology: Toxicology[]; //TODO: use to implement history
 
@@ -39,6 +43,7 @@ export class IngestionFormComponent implements OnInit {
     createForm() {
         this.ingestionForm = this.fb.group({
             concen: [null, this.validationService.nonNegative],
+            concenUnits: [this.concenUnitsMassVolOptions[0], Validators.required], //TODO: set initial value (how???), properly bind selection
             intake: [null, this.validationService.nonNegative],
             weight: [null, this.validationService.nonNegative],
             dose: [null, this.validationService.nonNegative]
@@ -49,12 +54,13 @@ export class IngestionFormComponent implements OnInit {
         this.pastToxicology = [];
         this.createForm();
         //this.toxicology = new Toxicology();//TODO: remove
-        this.concenUnit = new Toxicology(new ToxUnit(UnitTypes.CONCENTRATION, new ToxRatio('mg/L', 1)), null);
-        this.intakeUnit = new Toxicology(new ToxUnit(UnitTypes.INTAKE_RATE, new ToxRatio('L/day', 1)), null);
-        this.weightUnit = new Toxicology(new ToxUnit(UnitTypes.BODY_WEIGHT, new ToxRatio('kg', 1)), null);
-        this.doseUnit = new Toxicology(new ToxUnit(UnitTypes.DOSE, new ToxRatio('mg/kg BW/day', 1)), null);
+        this.concenTox = new Toxicology(new ToxUnit(UnitTypes.CONCENTRATION, new ToxRatio('mg/L', 1)), null);
+        this.intakeTox = new Toxicology(new ToxUnit(UnitTypes.INTAKE_RATE, new ToxRatio('L/day', 1)), null);
+        this.weightTox = new Toxicology(new ToxUnit(UnitTypes.BODY_WEIGHT, new ToxRatio('kg', 1)), null);
+        this.doseTox = new Toxicology(new ToxUnit(UnitTypes.DOSE, new ToxRatio('mg/kg BW/day', 1)), null);
+        //const selectedConcenUnit = this.concenUnitsMassVolOptions.find(concenUnits => concenUnits.value
     }
-    //TODO: change calculate function to accept new Toxicology object style
+
     calculate(): void {
         let solution = this.calcService.newCalculate(
             this.ingestionForm.get('concen').value,
@@ -63,10 +69,12 @@ export class IngestionFormComponent implements OnInit {
             this.ingestionForm.get('dose').value
         );
 
-        this.concenUnit.inputData = solution[0];
-        this.intakeUnit.inputData = solution[1];
-        this.weightUnit.inputData = solution[2];
-        this.doseUnit.inputData = solution[3];
+        //alert(this.ingestionForm.get('concenUnits').value.value);
+
+        this.concenTox.inputData = solution[0];
+        this.intakeTox.inputData = solution[1];
+        this.weightTox.inputData = solution[2];
+        this.doseTox.inputData = solution[3];
 
         //this.toxicology = this.calcService.calculate(this.toxicology);
         //let variable = this.toxicology.foundValue.variable;
@@ -75,10 +83,10 @@ export class IngestionFormComponent implements OnInit {
         //console.log(value);
 
         this.ingestionForm.patchValue({
-            concen: this.concenUnit.inputData,
-            intake: this.intakeUnit.inputData,
-            weight: this.weightUnit.inputData,
-            dose: this.doseUnit.inputData
+            concen: this.concenTox.inputData,
+            intake: this.intakeTox.inputData,
+            weight: this.weightTox.inputData,
+            dose: this.doseTox.inputData
         });
         this.submitted = true;
     }
