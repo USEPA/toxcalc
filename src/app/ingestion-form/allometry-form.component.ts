11eef6ce9713104f2bf03b5cff1bbfa9c373a1dc
RyanWYoung <ryan_w_young@hotmail.com>
2017-08-02 14:16:15 -0400

    added basic human equivalent dose calculation. Not sure how I'm going to expand it in order to be more precise

--- src/app/ingestion-form/allometry-form.component.ts
+++ src/app/ingestion-form/allometry-form.component.ts
@@ -4,6 +4,8 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 import { CalcService } from '../shared/calc.service';
 import { ValidationService } from '../shared/validation.service';
 
+declare let Math;
+
 @Component({
     selector: 'app-allometry-form',
     templateUrl: './allometry-form.component.html',
@@ -15,6 +17,16 @@ export class AllometryFormComponent implements OnInit {
     animalWeight: number;
     humanDose: number;
     humanWeight: number;
+    conversionFactor;
+
+    submitted = false;
+
+    conversionFactorOptions = [
+        {display: 'FDA Standard (0.33)', value: (1/3)},
+        {display: 'EPA Standard (0.25)', value: (1/4)}
+    ];
+
+    allometryForm: FormGroup;
 
     constructor(
         private fb: FormBuilder,
@@ -22,7 +34,36 @@ export class AllometryFormComponent implements OnInit {
         private validationService: ValidationService
     ) { }
 
+    createForm() {
+        this.allometryForm = this.fb.group({
+            animalDose: [null, [Validators.required, this.validationService.nonNegative]],
+            animalWeight: [null, [Validators.required, this.validationService.nonNegative]],
+            conversionFactor: [''],
+            humanDose: [null, this.validationService.nonNegative],
+            humanWeight: [null, [Validators.required, this.validationService.nonNegative]]
+        });
+
+        this.allometryForm.patchValue({
+            conversionFactor: this.conversionFactorOptions[0]
+        });
+    }
+
     ngOnInit() {
+        this.createForm();
+    }
+
+    hedConversion() {
+        this.animalDose = this.allometryForm.get('animalDose').value;
+        this.animalWeight = this.allometryForm.get('animalWeight').value;
+        this.humanWeight = this.allometryForm.get('humanWeight').value;
+        this.conversionFactor = this.allometryForm.get('conversionFactor').value.value;
+
+        let kRatio = this.animalWeight / this.humanWeight;
+
+        this.humanDose = this.animalDose * Math.pow(kRatio, this.conversionFactor);
+
+        this.submitted = true;
+
     }
 
 }
