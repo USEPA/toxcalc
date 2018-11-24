970af9e6786ea3b4c5ae2c1703e581dc0025d057
Nick Lewycky <nicholas@mxc.ca>
2018-11-23 20:05:23 -0800

    Add the three forms to the routing module. They don't match the entries in the landing page yet.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LandingComponent } from './landing/landing.component';
+import { AllometryFormComponent } from './ingestion-form/allometry-form.component';
+import { IngestionFormComponent } from './ingestion-form/ingestion-form.component';
+import { InhalationFormComponent } from './inhalation-form/inhalation-form.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: LandingComponent },
+  { path: 'ingestion', component: IngestionFormComponent },
+  { path: 'allometry', component: AllometryFormComponent },
+  { path: 'inhalation', component: InhalationFormComponent },
 ];
 
 @NgModule({
