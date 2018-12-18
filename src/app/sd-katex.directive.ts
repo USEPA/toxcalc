78fbf632a8a41ae27c4e1162dc6caf827758c3a2
Nick Lewycky <nicholas@mxc.ca>
2018-12-18 12:53:19 -0800

    Migrate equation snippets from MathJax to KaTeX.

--- src/app/sd-katex.directive.ts
+++ src/app/sd-katex.directive.ts
@@ -3,7 +3,8 @@ import { Directive, ElementRef, Input, OnChanges } from '@angular/core';
 import * as katex from 'katex';
 
 @Directive({
-  selector: '[sdKatex]'
+  selector: '[sdKatex]',
+  host: {'class': 'katex'}
 })
 export class SdKatexDirective implements OnChanges {
   @Input('sdKatex') private value: string;
