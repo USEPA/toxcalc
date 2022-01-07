057c6df99848e732101e7b901e3b4748c4e162c7
Nick Lewycky <nicholas@mxc.ca>
2022-01-06 16:10:27 -0800

    Make Field::unit and ::value functions with getters and setters, since they're overridden with methods.
    Migrate to using fontawesome icon library instead of the global icon library.
    
    Add angular/i18n for $localize because it is required by ng-bootstrap.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,7 @@
+/***************************************************************************************************
+ * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
+ */
+import '@angular/localize/init';
 // ToxCalc! by Safe Dose © 2018-2021. See LICENSE file for details.
 // SPDX-License-Identifier: GPL-3.0-or-later
 
